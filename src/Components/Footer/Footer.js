cf8fa20e9436da5cce9f97cf662fe426cedda359
Subash466 <2812subash@gmail.com>
2021-07-13 10:46:16 +0530

    Footer component updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="link">
-      <a href="https://github.com/Subash466/" ><span className="glyphicon glyphicon-log-in"> linkedin</span></a>
-      <a href="https://www.linkedin.com/in/subash-balraj/" ><span className="glyphicon glyphicon-export"> GitHub</span></a>
+      <a href="https://github.com/Subash466/" ><span className="glyphicon glyphicon-log-in"> github</span></a>      
+      <a href="https://www.linkedin.com/in/subash-balraj/" ><span className="glyphicon glyphicon-export"> linkedin</span></a>
       </div>
       <div className="info">
         <p><span className="glyphicon glyphicon-envelope"> 2812subash@gmail</span></p>
